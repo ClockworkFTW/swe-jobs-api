eca302aa752227f4da22f989a13451b2ec8741a2
John Boyle <nikolasboyle@gmail.com>
2022-07-12 16:03:00 -0700

    Add placeholder data for job salary and resume match

--- routes/job.js
+++ routes/job.js
@@ -2,9 +2,20 @@ import { Router } from "express";
 
 const router = Router();
 
+const randBetween = (min, max) =>
+  Math.floor(Math.random() * (max - min + 1)) + min;
+
 router.get("/", async (req, res) => {
-  const jobs = await req.models.Job.findAll();
-  return res.status(200).json(jobs);
+  let jobs = await req.models.Job.findAll();
+
+  jobs = jobs.map((job) => {
+    const match = randBetween(1, 99);
+    const min = randBetween(8, 12) * 10000;
+    const max = randBetween(14, 22) * 10000;
+    return { ...job.get(), match, salary: { min, max } };
+  });
+
+  return res.status(200).json({ jobs });
 });
 
 export default router;
